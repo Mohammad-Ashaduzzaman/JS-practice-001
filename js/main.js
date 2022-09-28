0bf1e95f9ac458206b6e1f0cf22e672f7e1f2d99
Mohammad-Ashaduzzaman <aschgf@gmail.com>
2022-09-28 11:51:09 +0600

    login section done

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.getElementById('login-button').addEventListener('click',function(){
+    const userIdElement = document.getElementById('user-id');
+    const userId= userIdElement.value;
+    
+    const userPasswordElement = document.getElementById('user-password');
+    const userPassword= userPasswordElement.value;
+
+    if (userId==='bankers@bank.org' && userPassword==='secrete') {
+        window.location.href = 'account.html';
+    }else{
+        alert('Invalid user ID or Pass');
+    }
+})
